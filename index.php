1adfdbf6131bdd4408ffb93c0407ecb7157f0cde
SHRIKANTSHRI99606986070 <94349886+SHRIKANTSHRI99606986070@users.noreply.github.com>
2021-11-15 14:45:51 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sliver spoon admin</title>
+    <link rel="stylesheet" href= "images\admin\styel.css"      >
+</head>
+<body>
+    
+<div class="menu">
+    <div class="wrapper">
+        menu goes here bfgf
+       </div>
+    </div>
+    <div class="menu">
+        menu goes here
+    </div>
+    <div class="menu">
+        menu goes here
+    </div>    
+    
+</body>
+</html>
